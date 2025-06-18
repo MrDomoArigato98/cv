fc967aa4b9db05d970082ba66f94a2963b4d2801
MrDomoArigato98 <domdob98@gmail.com>
2025-06-18 15:20:56 +0100

    Creates project list of objects and populates home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,21 @@ import "../styles/home.css";
 
 import todo from "../assets/todo.png";
 export default function Home() {
+  const projects = [
+    {
+      title: "Todo Web app",
+      description: "abcd",
+      image: todo,
+      link: "https://mrdomoarigato98.github.io/The-Odin-Project/To-do%20App/dist/index.html",
+    },
+    {
+      title: "Dummy item",
+      description: "Another dummy item",
+      image: todo,
+      link: "",
+    },
+  ];
+
   return (
     <>
       <main className="bounce-in">
@@ -27,11 +42,21 @@ export default function Home() {
         </section>
 
         <div className="projects">
-          <div className="projects-row">
-            <p>Hey</p>
-            <img className="project-img" src={todo}></img>
-          </div>
-          <div className="projects-row">Todo </div>
+          <h3>Projects </h3>
+
+          {projects.map((item, idx) => {
+            return (
+              <div id={idx}>
+                <a className="projects-row" href={item.link}>
+                  <div className="projects-text">
+                    <h4>{item.title}</h4>
+                    <p className="project-description">{item.description}</p>
+                  </div>
+                  <img className="project-img" src={item.image}></img>
+                </a>
+              </div>
+            );
+          })}
           <div className="projects-row">Todo </div>
         </div>
       </main>
